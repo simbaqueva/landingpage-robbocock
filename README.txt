2d72f127412aeab45436a1bfd7cbce622339fe39
unknown <alvaroalexandersimbaqueva96@gmail.com>
2021-11-02 14:24:51 -0500

    landing page version 1.0

---
+++ README.txt
@@ -0,0 +1,5 @@
+la carpeta landing page debe estar en ruta  xampp htdocs 
+iniciar el servidor y correr el archivo index.html
+pulsar en start apache y mysql en el xampp panel
+y en el navegador poner http://localhost/landingpage/index.html
+y listo 
---
+++ js/modal.js
@@ -0,0 +1,26 @@
+var dialogo = document.getElementById('DialogoNotificacion');
+dialogo.showModal();
+
+
+
+function myFunction() {
+    document.getElementById("DialogoNotificacion").close(); 
+    } ;
+  
+
+  function Salida() {
+    window.location.assign("https://www.google.com");
+    }
+
+    function Proximamente() {
+      alert("¡PROXIMAMENTE!")
+      }
+  
+
+
+   
+/* function contactbtn() {
+      document.getElementById("contact_btn").classList.add("contact_btn_onclick");
+    }
+contactbtn();
+typeof contactbtn; */
